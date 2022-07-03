049bca9f70d88eaa1ccedc8dad970649f17d0ac6
khnychkin_kirill <khnychkin_kirill@tutanota.com>
2022-07-04 00:46:56 +0300

    added basic response with html page;

--- 01 - Node.js Basics/app.js
+++ 01 - Node.js Basics/app.js
@@ -1,10 +1,16 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-	console.log(req)
-	console.log(res)
+	console.log(req.url)
+	console.log(req.method)
 
-	return res;
+	res.setHeader('Content-type', 'text-html')
+	res.write('<html lang="en US">')
+	res.write('<head><title>Hello World!</title></head>')
+	res.write('<body><h1>Hello World!</h1></body>')
+	res.write('</html>')
+
+	res.end();
 })
 
 server.listen(4000)
