abc3c96e843034d8f9d3366362e8813320d22409
khnychkin_kirill <khnychkin_kirill@tutanota.com>
2022-07-05 00:28:39 +0300

    added basic routing;

--- 01 - Node.js Basics/app.js
+++ 01 - Node.js Basics/app.js
@@ -1,14 +1,32 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-	console.log(req.url)
-	console.log(req.method)
+	const url = req.url;
+	const method = req.method;
 
-	res.setHeader('Content-type', 'text-html')
-	res.write('<html lang="en US">')
-	res.write('<head><title>Hello World!</title></head>')
-	res.write('<body><h1>Hello World!</h1></body>')
-	res.write('</html>')
+	if (url === '/') {
+		res.write('<html lang="en US">');
+		res.write('<head><title>Enter text</title></head>');
+		res.write('<body><form action="/message" method="POST"><input type="text" /><button type="submit">btn</button> </form></body>');
+		res.write('</html>');
+
+		return res.end();
+	}
+
+	if (url === '/message' && method === 'POST') {
+		fs.writeFileSync('message.txt', 'placeholder');
+		res.statusCode = 302;
+		res.setHeader('Location', '/');
+
+		return res.end();
+	}
+
+	res.setHeader('Content-type', 'text-html');
+	res.write('<html lang="en US">');
+	res.write('<head><title>Hello World!</title></head>');
+	res.write('<body><h1>Hello World!</h1></body>');
+	res.write('</html>');
 
 	res.end();
 })
