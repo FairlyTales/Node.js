6350b9331736ad773f1ae5afc76fa12ba32bc438
khnychkin_kirill <khnychkin_kirill@tutanota.com>
2022-07-08 22:53:56 +0300

    added shop view and route;

--- 02 - Express.js/app.js
+++ 02 - Express.js/app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/admin', adminRoutes);
+app.use('/shop', shopRoutes);
 
 app.use('/', (req, res, next) => {
 	res.status(404).send('<h1>Page not found</h1>');
@@ -15,4 +17,4 @@ app.use('/', (req, res, next) => {
 	next();
 });
 
-app.listen(4000);
+app.listen(3000);
