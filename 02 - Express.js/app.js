6c2dfd454a0d3c42a6b52a160fd678d0625024cf
khnychkin_kirill <khnychkin_kirill@tutanota.com>
2022-07-08 22:16:28 +0300

    implemented post route and 404 handler;

--- 02 - Express.js/app.js
+++ 02 - Express.js/app.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((res, req, next) => {
-	console.log('res')
-	console.log(res)
-	console.log('req')
-	console.log(req)
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/form', (req, res, next) => {
+	console.log(req.body);
+	res.status(200).send('data received: ' + req.body.field.toString())
+});
+
+app.use('/', (req, res, next) => {
+	res.status(404).send('<h1>Page not found</h1>');
 
 	next();
 });
