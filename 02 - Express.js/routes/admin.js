3c43391521a3e992e20c68fee4c54f6ae257723b
khnychkin_kirill <khnychkin_kirill@tutanota.com>
2022-07-08 22:53:28 +0300

    added add-product view;

--- 02 - Express.js/routes/admin.js
+++ 02 - Express.js/routes/admin.js
@@ -1,17 +1,13 @@
 const express = require('express');
 const fs = require("fs");
+const path = require("path");
 
 const router = express.Router();
 
 router.get('/add-product', (req, res) => {
 	console.log(req.body);
 
-	res.status(200).send(
-		`<form action="/product" method="post">
-			<input type="text" name="title">
-			<button type="submit">submit</button>
-		</form>`
-	)
+	res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'));
 });
 
 router.post('/add-product', async (req, res) => {
