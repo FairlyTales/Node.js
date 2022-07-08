016611cd007729fb26c75d97f856eace75ce0277
khnychkin_kirill <khnychkin_kirill@tutanota.com>
2022-07-08 22:32:06 +0300

    moved shop admin routes to the separate file; implemented form data receiving routes and storage;

--- 02 - Express.js/app.js
+++ 02 - Express.js/app.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const adminRoutes = require('./routes/admin');
+
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.post('/form', (req, res, next) => {
-	console.log(req.body);
-	res.status(200).send('data received: ' + req.body.field.toString())
-});
+app.use(adminRoutes);
 
 app.use('/', (req, res, next) => {
 	res.status(404).send('<h1>Page not found</h1>');
---
+++ 02 - Express.js/routes/admin.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const fs = require("fs");
+
+const router = express.Router();
+
+router.get('/product-form', (req, res) => {
+	console.log(req.body);
+
+	res.send(
+		`<form action="/product" method="post">
+			<input type="text" name="title">
+			<button type="submit">submit</button>
+		</form>`
+	)
+});
+
+router.post('/add-product', async (req, res) => {
+	console.log(req.body);
+
+	res.send(`product ${req.body.title} added`);
+	await fs.writeFile('./database/products.txt', req.body.title, (err) => {
+		console.log(err)
+	})
+})
+
+module.exports = router;
